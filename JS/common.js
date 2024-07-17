e48b1dc69433bf75bb82dc4b1057d8c15020fd12
stephanie <j0102276@gmail.com>
2024-07-17 22:17:19 +0900

    test

---
+++ JS/common.js
@@ -0,0 +1,38 @@
+$(document).ready( function() {
+        
+  $("#header").load("common/header.html"); 
+  $("#nav").load("common/nav.html");  
+  $("#footer").load("common/footer.html");  
+  
+  });
+
+$(".book_list--roll").slick({
+    autoplay: true,
+    arrows:true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,			
+    autoplaySpeed : 3000,
+    responsive: [ 
+    {  
+      breakpoint: 1500, 
+      settings: {
+        slidesToShow: 5
+      } 
+    },
+    {  
+      breakpoint: 1400, 
+      settings: {
+        slidesToShow: 4
+      } 
+    },
+    { 
+      breakpoint: 980, 
+      settings: {    
+        slidesToShow: 2
+      } 
+    }
+  ] 
+  });  
+  
