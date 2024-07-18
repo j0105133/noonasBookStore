3be14e639b348b2e3067bbbe256f987ceb5e80c1
Hyejin Kim <khyejin08@naver.com>
2024-07-18 15:00:27 +0900

    베스트셀러 탭 메뉴 완성

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,16 @@ const API_KEY = `ttbqhfhfh3691231001`
 let mdBookList = [];
 let bestBookList = [];
 let newBookList = [];
+let tabBtn = document.querySelectorAll('.tab__item');
+
+
+
+// 각 탭 버튼에 베스트셀러 랜더 함수 부착
+tabBtn.forEach(item => item.addEventListener('click', (event) => getBestBookRender(event)));
 
 
 // 국내도서
+// 최초 화면 렌더링
 function getBookData (){
   // 신간
   var newBookUrl = `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=ItemNewAll&MaxResults=6&start=1&SearchTarget=Book&output=js&Version=20131101&callback=newBookDisplay`
@@ -40,6 +47,10 @@ function getBookData (){
 getBookData();
 
 
+
+
+
+
 // 신간 display 함수
 function newBookDisplay(success) {
   console.log("new 전체 데이터", success);
@@ -48,12 +59,11 @@ function newBookDisplay(success) {
   newBookRender();
 }
 
-
 // 신간 render 함수
 function newBookRender (){
   let newBookHTML = ``;
   newBookHTML = newBookList.map(book => `
-    <div class="book__item ">
+    <div class="book__item">
       <span class="book__img"><img class="bookImgSize" src=${book.cover} /></span>
       <dl class="book__text">
           <dt>${book.title}</dt>
@@ -62,6 +72,19 @@ function newBookRender (){
     </div>`).join('');
 
   document.getElementById('book_list--roll').innerHTML = newBookHTML;
+}
+
+
+
+
+
+
+// 베스트셀러 탭 메뉴 클릭 이벤트
+for (let i = 0; i < tabBtn.length; i++){
+  $('.tab__item').eq(i).on('click', function(){
+    $('.tab__item').removeClass('tab__item--active'); 
+    $('.tab__item').eq(i).addClass('tab__item--active');
+  });
 }
 
 
@@ -78,7 +101,7 @@ function bestBookDisplay(success) {
 function bestBookRender (){
   let bestBookHTML = ``;
   bestBookHTML = bestBookList.map(book => `
-    <div class="book__item">
+    <div class="book__item tab__book__item tab__book__display__none show">
       <div class="book__img"><img class="bookImgSize" src=${book.cover} /></div>
       <dl class="book__text">
           <dt>${book.title}</dt>
@@ -88,6 +111,44 @@ function bestBookRender (){
 
   document.getElementById('book__list').innerHTML = bestBookHTML;
 }
+
+
+// 베스트셀러 파트 탭 메뉴 함수
+function getBestBookRender (event){
+  let category = event.target.textContent;
+  if (category == '건강/취미'){
+      var bestBookUrl = `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=Bestseller&MaxResults=4&start=1&SearchTarget=Book&output=js&Version=20131101&callback=bestBookDisplay&CategoryId=53476`
+
+      // 베스트셀러
+      $.ajax({
+        url: bestBookUrl,
+        jsonp: "bestBookDisplay",
+        dataType: "jsonp"
+      });
+  } else if (category == '여행'){
+      var bestBookUrl = `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=Bestseller&MaxResults=4&start=1&SearchTarget=Book&output=js&Version=20131101&callback=bestBookDisplay&CategoryId=51377`
+
+      // 베스트셀러
+      $.ajax({
+        url: bestBookUrl,
+        jsonp: "bestBookDisplay",
+        dataType: "jsonp"
+      });
+  } else if (category == '인문학'){
+    var bestBookUrl = `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${API_KEY}&QueryType=Bestseller&MaxResults=4&start=1&SearchTarget=Book&output=js&Version=20131101&callback=bestBookDisplay&CategoryId=656`
+
+      // 베스트셀러
+      $.ajax({
+        url: bestBookUrl,
+        jsonp: "bestBookDisplay",
+        dataType: "jsonp"
+      });
+  }
+}
+
+
+
+
 
 
 // 편집자 추천 도서 display 함수
