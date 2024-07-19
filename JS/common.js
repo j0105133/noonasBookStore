55bdfb361a6e383b9f1ed25ec28d59fb834cfdca
stephanie <j0102276@gmail.com>
2024-07-19 20:59:58 +0900

    search save

--- JS/common.js
+++ JS/common.js
@@ -6,33 +6,33 @@ $(document).ready( function() {
   
   });
 
-$(".book_list--roll").slick({
-    autoplay: true,
-    arrows:true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,			
-    autoplaySpeed : 3000,
-    responsive: [ 
-    {  
-      breakpoint: 1500, 
-      settings: {
-        slidesToShow: 5
-      } 
-    },
-    {  
-      breakpoint: 1400, 
-      settings: {
-        slidesToShow: 4
-      } 
-    },
-    { 
-      breakpoint: 980, 
-      settings: {    
-        slidesToShow: 2
-      } 
-    }
-  ] 
-  });  
+// $(".book_list--roll").slick({
+//     autoplay: true,
+//     arrows:true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 5,
+//     slidesToScroll: 1,			
+//     autoplaySpeed : 3000,
+//     responsive: [ 
+//     {  
+//       breakpoint: 1500, 
+//       settings: {
+//         slidesToShow: 5
+//       } 
+//     },
+//     {  
+//       breakpoint: 1400, 
+//       settings: {
+//         slidesToShow: 4
+//       } 
+//     },
+//     { 
+//       breakpoint: 980, 
+//       settings: {    
+//         slidesToShow: 2
+//       } 
+//     }
+//   ] 
+//   });  
   
