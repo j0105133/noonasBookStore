26b40f8082f26c7e2c7c249ab31f97678b16d59e
sueyesi <publisher724@gmail.com>
2024-07-20 22:56:05 +0900

    20240720_2

--- JS/common.js
+++ JS/common.js
@@ -1,10 +1,4 @@
-$(document).ready( function() {
-        
-  $("#header").load("common/header.html"); 
-  $("#nav").load("common/nav.html");  
-  $("#footer").load("common/footer.html");  
-  
-  });
+
 
 // $(".book_list--roll").slick({
 //     autoplay: true,
